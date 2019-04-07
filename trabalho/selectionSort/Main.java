4920e721b3cc6c59df72da3549879f86261e9e10
Heitor Batistela Zunta <heitor@basenetworks.com.br>
2019-04-07 13:03:17 -0400

    #1

---
+++ trabalho/selectionSort/Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int cardinalidade;
+		
+		String[] palavras;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		cardinalidade = sc.nextInt();
+		sc.nextLine();
+		
+		palavras = new String[cardinalidade];
+		
+		for(int i=0; i<cardinalidade; i++) {
+			palavras[i] = sc.nextLine();	
+		}
+		
+//		bubbleSort(palavras);
+		selectionSort(palavras);
+		
+		print(palavras);
+		
+		sc.close();		
+
+	}
+
+	private static void selectionSort(String[] palavras) {
+		for(int i=0; i<palavras.length-1; i++) {
+			int min = i;
+			for(int j=i+1; j<palavras.length; j++) {
+				if(palavras[min].compareTo(palavras[j])>0) {
+					min = j;
+				}
+				if(min!=i) swap(palavras, min, i);
+			}
+		}
+		
+	}
+
+	private static void print(String[] palavras) {
+		for(int i=0; i<palavras.length; i++) {
+			System.out.print(palavras[i] + " ");
+		}
+		
+	}
+
+	private static void bubbleSort(String[] palavras) {
+		
+		for(int i=0; i<palavras.length-1; i++) {
+			for(int j=0; j<palavras.length-1; j++) {
+				
+				if(palavras[j].compareTo(palavras[j+1])>0)
+					swap(palavras, j, j+1);				
+			}
+		}
+		
+	}
+
+	private static void swap(String[] string, int i, int j) {
+		String aux;
+		
+		aux= string[i];
+		string[i] = string[j];
+		string[j] = aux;	
+	}
+
+}
